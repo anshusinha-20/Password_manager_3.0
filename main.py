550eb05decea01365abc1ff10ea876f65c2811be
Anshu Sinha <anshujuly2@gmail.com>
2023-04-08 20:17:21 +0530

    Add a code to raise exception

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-try:
-    file = open("file1.txt")
-    dict = {1: "Anshu"}
-    print(dict[1])
+# try:
+#     file = open("file1.txt")
+#     dict = {1: "Anshu"}
+#     print(dict[1])
+#
+# except FileNotFoundError:
+#     file = open("file1.txt", mode="w")
+#     file.write("Something")
+#
+# except KeyError as errorMessage:
+#     print(f"The key {errorMessage} doesn't exist")
+#
+# else:
+#     contents = file.read()
+#     print(contents)
+#
+# finally:
+#     file.close()
+#     print("file was closed")
 
-except FileNotFoundError:
-    file = open("file1.txt", mode="w")
-    file.write("Something")
+"""raising exceptions"""
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kilograms: "))
 
-except KeyError as errorMessage:
-    print(f"The key {errorMessage} doesn't exist")
+if height > 3:
+    raise ValueError("Human height shouldn't be more than 3 meters")
 
-else:
-    contents = file.read()
-    print(contents)
-
-finally:
-    file.close()
-    print("file was closed")
+bmi = weight / height ** 2
+print(bmi)
